c4048c7490bd9a1c950939bd0d96b2ec7154b116
Allan Milne <amilne.wd@gmail.com>
2019-10-30 16:01:24 +0000

    mvp done

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -4,38 +4,54 @@ class FormComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      salary: "",
-      propertyValue: ""
+      firstSalary: 0,
+      secondSalary: 0,
+      propertyValue: 0,
+      totalSalary: 0
     };
-    this.handleSalaryChange = this.handleSalaryChange.bind(this);
+    this.handleFirstSalaryChange = this.handleFirstSalaryChange.bind(this);
+    this.handleSecondSalaryChange = this.handleSecondSalaryChange.bind(this);
     this.handlePropertyValueChange = this.handlePropertyValueChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSalaryChange(event) {
-    this.setState({ salary: event.target.value });
+  handleFirstSalaryChange(event) {
+    this.setState({ firstSalary: event.target.value });
+  }
+
+  handleSecondSalaryChange(event) {
+    this.setState({ secondSalary: event.target.value });
   }
 
   handlePropertyValueChange(event) {
     this.setState({ propertyValue: event.target.value });
   }
 
+  // sumOfSalaries() {
+  //   const totalSalary = this.state.firstSalary + this.state.secondSalary;
+  //   this.setState({totalSalary: totalSalary});
+  // }
+
   handleSubmit(event) {
     event.preventDefault();
-    const salary = this.state.salary.trim();
+    const firstSalary = this.state.firstSalary.trim();
+    const secondSalary = this.state.secondSalary.trim();
     const propertyValue = this.state.propertyValue.trim();
-    if (!salary || !propertyValue) {
+    if (!firstSalary || !secondSalary || !propertyValue) {
       return;
-    };
-    console.log("hello");
+    }
+
+    const totalSalary =
+      parseInt(this.state.firstSalary) + parseInt(this.state.secondSalary);
+    this.setState({totalSalary: totalSalary})
 
     let canAfford = false;
-    if (salary * 3 > propertyValue) {
+    console.log("total salary", totalSalary);
+    if (totalSalary * 3 >= propertyValue) {
       canAfford = true;
-    };
-    console.log("can afford", canAfford);
-    this.props.onFormSubmit( canAfford );
-    this.setState({ salary: "", propertyValue: "" });
+    }
+    this.props.onFormSubmit(canAfford);
+    this.setState({ firstSalary: "", secondSalary: "", propertyValue: "" });
   }
 
   render() {
@@ -44,14 +60,17 @@ class FormComponent extends Component {
         <form className="form" onSubmit={this.handleSubmit}>
           <input
             type="text"
-            placeholder="salary"
-            value={this.state.salary}
-            onChange={this.handleSalaryChange}
+            placeholder="First Salary"
+            onChange={this.handleFirstSalaryChange}
+          ></input>
+          <input
+            type="text"
+            placeholder="Second Salary"
+            onChange={this.handleSecondSalaryChange}
           ></input>
           <input
             type="text"
             placeholder="property value"
-            value={this.state.propertyValue}
             onChange={this.handlePropertyValueChange}
           ></input>
           <input type="submit" value="Calculate" />
